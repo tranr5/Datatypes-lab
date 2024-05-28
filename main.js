b9d62555a60741efe0c02fe74a3bad6e9b6cf4c7
tranr5 <tranr4@gmail.com>
2024-05-28 14:10:22 -0400

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+let myFavoriteNumber = 4
+
+// console.log(myFavoriteNumber)
+let myNumber = 10
+myNumber = myNumber - 4
+myNumber = myNumber * 3
+myNumber = myNumber / 6
+
+console.log(myNumber)
+
